6f6eb1435ff1ccbfae131a3e982a4afeee78eeac
Justin Forsyth <justin@forsyth.im>
2019-12-27 12:16:08 -0500

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,54 @@ mod d3;
 mod d4;
 mod intcode;
 
+#[cfg(test)]
+mod tests {
+    use crate::d1;
+    use crate::d2;
+    use crate::d3;
+    use crate::d4;
+
+    #[test]
+    fn test_d1_p1() {
+        assert_eq!(d1::part1().unwrap(), 3382136);
+    }
+
+    #[test]
+    fn test_d1_p2() {
+        assert_eq!(d1::part2().unwrap(), 5070314);
+    }
+
+    #[test]
+    fn test_d2_p1() {
+        assert_eq!(d2::part1().unwrap(), 4462686);
+    }
+
+    #[test]
+    fn test_d2_p2() {
+        assert_eq!(d2::part2().unwrap(), 5936);
+    }
+
+    #[test]
+    fn test_d3_p1() {
+        assert_eq!(d3::part1().unwrap(), 1211);
+    }
+
+    #[test]
+    fn test_d3_p2() {
+        assert_eq!(d3::part2().unwrap(), 101386);
+    }
+
+    #[test]
+    fn test_d4_p1() {
+        assert_eq!(d4::part1(), 979);
+    }
+
+    #[test]
+    fn test_d4_p2() {
+        assert_eq!(d4::part2(), 635);
+    }
+}
+
 fn main() {
     let res_d1_p1 = d1::part1().unwrap();
     println!("d1_p1: {}", res_d1_p1);
